71146b571df3519caefe81d8aa808f7439d22099
Sabino Velasquez <sabino@2brains.cl>
2018-05-29 16:12:42 -0400

    to the world

---
+++ src/components/Landing.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  section.hero.is-fullheight.landing.is-desktop(style='background-image:url("../assets/img/bg-top.jpg")')
+    .hero-body
+      .container.has-text-centered
+        .columns.is-desktop.is-centered.is-vcentered
+          .column
+            img.main-logo(src='../assets/logos/front.svg')
+            h1.title _front
+            h2.subtitle Soluciones
+            p.is-centered.buttons
+              a.button.is-primary(href='mailto:marcos@front.cl')
+                span.icon
+                  i.far.fa-paper-plane
+                span Contáctanos
+        .columns.inner.is-mobile.is-centered.is-vcentered
+          .column.is-one-quarter-mobile
+            img.logo(src='../assets/logos/vue.svg', alt='Vue')
+          .column.is-one-quarter-mobile
+            img.logo(src='../assets/logos/meteor.svg', alt='Meteor')
+          .column.is-one-quarter-mobile
+            img.logo(src='../assets/logos/ember.svg', alt='Ember')
+          .column.is-one-quarter-mobile
+            img.logo(src='../assets/logos/angular.svg', alt='Angular')
+          .column.is-one-quarter-mobile
+            img.logo(src='../assets/logos/react.svg', alt='React')
+</template>
+
+<style lang="scss">
+  @import '../assets/sass/landing.scss';
+</style>
+
+<script>
+export default {
+  name: 'Landing'
+}
+</script>
