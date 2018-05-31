8d07a024590c4496de675d1ac5c988835a3abdea
Sabino Velasquez <sabinovelasquez@gmail.com>
2018-05-30 21:57:25 -0400

    first pricing approach

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from '@/components/Landing'
+import Scanpad from '@/components/Scanpad'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Landing',
       component: Landing
+    },
+    {
+      path: '/scanpad',
+      name: 'Scanpad',
+      component: Scanpad
     }
   ]
 })
